c41f3e121f2e2afdeee03c33522af27c162d145c
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2025-02-25 13:01:10 -0600

    Base

---
+++ app/__init__.py
@@ -0,0 +1,4 @@
+from .database import init_db
+from .utils import get_heating_oil
+
+__all__ = ["init_db", "get_heating_oil"]
---
+++ app/main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from app import init_db
+import logging
+from .routers import auth, user, getHeating,forgotPassword
+from .createAdmin import createAdmin
+
+
+init_db()
+app = FastAPI()
+origins = [ 
+    "https://getb99.streamlit.app",    # Add production domain here
+]
+
+# Add CORSMiddleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(auth.router)
+app.include_router(user.router)
+app.include_router(getHeating.router)
+app.include_router(forgotPassword.router)
+
+@app.on_event("startup")
+async def startup_event():
+    if createAdmin():
+        logging.info("ADMIN Created!!")
+    else:
+        logging.info("ADMIN already Exits!!")
