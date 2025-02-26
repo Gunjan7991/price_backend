53396b0371db9320e1dd2dab6ed2eeea0ce0c074
Gunjan Basnet <65468162+Gunjan7991@users.noreply.github.com>
2025-02-26 06:07:08 -0600

    hot fix 3

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ origins = [
 # Add CORSMiddleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    # allow_origins=origins,
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
